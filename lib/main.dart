0e9219d40dde4cddd144c4dd9a4eac8b720427b9
roshan-lab <roshq.1234@gmail.com>
2022-08-06 19:05:12 +0530

    Place order page created

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:devthread/pages/payment_page.dart';
 import 'package:devthread/pages/paymentcomfimation_page.dart';
 import 'package:devthread/pages/prod_Description.dart';
 import 'package:devthread/pages/product_Info.dart';
+import 'package:devthread/pages/placeorder_page.dart';
 import 'package:devthread/pages/services_popup.dart';
 import 'package:devthread/pages/signup_page.dart';
 import 'package:devthread/pages/total_amount.dart';
@@ -44,7 +45,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
 
 
-      home: PaymentComplete(),
+      home: SelectDomain(),
 
       //home: ForgotPassword(),
 
